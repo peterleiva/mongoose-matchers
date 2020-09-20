4bd8c2bc2b9db48fd8d4105fcc222b0e27818ec5
Peter Leiva <pleiva@id.uff.br>
2020-09-20 20:22:23 -0300

    Use nodeExternals and add utils non-relative path

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
 	entry: './lib/index.ts',
+	target: 'node',
 	module: {
 		rules: [
 			{
@@ -13,12 +15,15 @@ module.exports = {
 		],
 	},
 
+	externals: [nodeExternals()],
+
 	plugins: [new CleanWebpackPlugin()],
 
 	resolve: {
 		extensions: ['.tsx', '.ts', '.js'],
 		alias: {
 			lib: path.join(__dirname, 'lib'),
+			utils: path.join(__dirname, 'lib', 'utils'),
 		},
 	},
 
